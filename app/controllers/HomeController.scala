fc6ddea6eb49c50479cf2dba301ab94980580243
m-r-poole-dev <55984889+m-r-poole-dev@users.noreply.github.com>
2021-03-17 09:16:03 +0000

    add basic views

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,13 +3,16 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import views.html.Index
+
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(mcc: MessagesControllerComponents, indexView: Index) extends FrontendController(mcc) {
 
   /**
    * Create an Action to render an HTML page.
@@ -18,7 +21,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index: Action[AnyContent] = Action { implicit request =>
+    Ok(indexView())
   }
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,4 @@
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.23")
 addSbtPlugin("org.foundweekends.giter8" % "sbt-giter8-scaffold" % "0.11.0")
+addSbtPlugin("com.typesafe.sbt" % "sbt-twirl" % "1.3.0")
 resolvers += "HMRC Releases" at "https://dl.bintray.com/hmrc/releases"
